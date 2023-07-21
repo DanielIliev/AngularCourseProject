198947d3e914c2e78c1e003c1c355688615755b8
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-07-21 13:49:28 +0300

    Update: Custom navbar button + SCSS variables

--- forum-app/src/app/app.component.ts
+++ forum-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'forum-app';
+export class AppComponent implements OnInit {
+  loggedIn: boolean = false;
+  active: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // TODO verify if the used is already logged in
+  }
+
+  toggleIconAnimation() {
+    // TODO hide collapsed menu on nav click
+    this.active = !this.active;
+  }
 }
--- forum-app/src/app/core/core.module.ts
+++ forum-app/src/app/core/core.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
-
-
 @NgModule({
   declarations: [
     HomeComponent,
