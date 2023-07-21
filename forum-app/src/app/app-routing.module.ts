9b1ac7bbced7c146e28c0f4732edae35f6695311
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-07-21 12:29:32 +0300

    Update: 404 page

--- forum-app/src/app/app-routing.module.ts
+++ forum-app/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
 
 const routes: Routes = [
   { path: '', title: 'Home', component: HomeComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', component: NotfoundComponent }
 ];
 
 @NgModule({
--- forum-app/src/app/core/core.module.ts
+++ forum-app/src/app/core/core.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HomeComponent
+    HomeComponent,
+    NotfoundComponent
   ]
 })
 export class CoreModule { }
