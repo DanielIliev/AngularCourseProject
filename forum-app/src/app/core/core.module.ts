fcf5fe9ef76c266622edbc3e4e1151d3c0781777
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-07-24 11:35:19 +0300

    Update: home page

--- forum-app/src/app/core/core.module.ts
+++ forum-app/src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { NotfoundComponent } from './notfound/notfound.component';
     NotfoundComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     HomeComponent,
