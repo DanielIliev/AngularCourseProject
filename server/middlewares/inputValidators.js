dc5306a54a947f66ca6a29874472e540b99bc3bf
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-07-28 14:46:44 +0300

    Update: Input validators

--- server/middlewares/inputValidators.js
+++ server/middlewares/inputValidators.js
@@ -1,40 +1,53 @@
 const { check } = require('express-validator');
 
+// Authentication validators
 const usernameValidators =
     check('username')
-        .not().isEmpty().withMessage('Username is required!')
+        .escape()
+        .not()
+        .isEmpty()
+        .withMessage('Username is required!')
         .isLength(
             {
                 min: 3,
                 max: 25
             }
-        ).withMessage('Username must be between 3 and 25 characters long!')
+        )
+        .withMessage('Username must be between 3 and 25 characters long!')
+        .matches(/^[A-Za-z0-9]+$/)
+        .withMessage('Only English letters and numbers are allowed for the username')
         .trim()
-        .escape()
 
 const emailValidators =
     check('email')
-        .not().isEmpty().withMessage('Email is required!')
-        .isEmail().withMessage('Email must be a valid email address!')
-        .trim()
         .escape()
+        .not()
+        .isEmpty()
+        .withMessage('Email is required!')
+        .isEmail()
+        .withMessage('Email must be a valid email address!')
+        .trim()
 
 const passwordValidators =
     check('password')
-        .not().isEmpty().withMessage('Password is required!')
+        .escape()
+        .not()
+        .isEmpty()
+        .withMessage('Password is required!')
         .isLength({
             min: 4
-        }).withMessage('Passwords must be at least 4 characters long!')
+        })
+        .withMessage('Passwords must be at least 4 characters long!')
         .trim()
-        .escape()
 
 
 const repeatPasswordValidators =
     check('repass')
-        .not().isEmpty()
+        .escape()
+        .not()
+        .isEmpty()
         .withMessage('Repeat password is required!')
         .trim()
-        .escape()
 
 const registerValidators = [
     usernameValidators,
@@ -43,20 +56,24 @@ const registerValidators = [
     repeatPasswordValidators
 ];
 
+// Content validators
 const postValidators = [
     check('title')
-        .not().isEmpty()
+        .escape()
+        .not()
+        .isEmpty()
         .withMessage('Post title is required!')
         .isLength({
             min: 3,
             max: 25
         })
         .withMessage('Post title must be between 3 and 15 characters long!')
-        .trim()
-        .escape(),
+        .trim(),
 
     check('content')
-        .not().isEmpty({ ignore_whitespace: false })
+        .escape()
+        .not()
+        .isEmpty({ ignore_whitespace: false })
         .withMessage('Post content is required!')
         .isLength({
             min: 10,
@@ -64,21 +81,26 @@ const postValidators = [
         })
         .withMessage('Post content must be between 10 and 350 characters long')
         .trim()
-        .escape()
 ];
 
 const commentValidators = [
     check('username')
-    .not().isEmpty()
-    .withMessage('Unable to add your comment'),
+        .escape()
+        .not()
+        .isEmpty()
+        .withMessage('Unable to add your comment')
+        .trim(),
+        
     check('comment')
-    .not().isEmpty()
-    .withMessage('The comment field is required!')
-    .isLength({
-        max: 350
-    }).withMessage('Your comment exceeds the maximum length of 350 characters')
-    .trim()
-    .escape()
+        .escape()
+        .not()
+        .isEmpty()
+        .withMessage('The comment field is required!')
+        .isLength({
+            max: 350
+        })
+        .withMessage('Your comment exceeds the maximum length of 350 characters')
+        .trim()
 ];
 
 module.exports = {
