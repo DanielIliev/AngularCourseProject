b68ac0e2c65e3b545463d6f9132296b41e0d23ae
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-08-10 14:01:45 +0300

    Update

--- forum-app/src/app/core/home/home.component.ts
+++ forum-app/src/app/core/home/home.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
+export class HomeComponent implements OnInit {
+  isLogged: boolean = false;
+  constructor(private router: Router, private localStorageService: LocalStorageService) {}
+
+  ngOnInit(): void {
+    const token = this.localStorageService.get('authToken');
+
+    if (token !== null) {
+      this.isLogged = true;
+    }
+  }
 
   goToPosts() {
     this.router.navigate(['/board']);
+  }
+
+  goToAddPost() {
+    this.router.navigate(['/add']);
   }
 
   goToLogin() {
