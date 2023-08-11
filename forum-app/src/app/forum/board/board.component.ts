85a6b6faf2aa5405b35ae8328bb8d9f9874eb897
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-08-11 15:08:07 +0300

    Update

--- forum-app/src/app/forum/board/board.component.ts
+++ forum-app/src/app/forum/board/board.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BoardService } from './board.service';
 import { Post } from 'src/app/types/Post';
 import { Router } from '@angular/router';
+import { Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-board',
@@ -12,22 +13,31 @@ export class BoardComponent implements OnInit {
   isLoading: boolean = true;
   posts: Post[] = [];
   errorMessage: string = '';
+  boardSub: Subscription | undefined;
+  boardObs: Observer<Post[]> = {
+    next: (response) => {
+      this.posts = response;
+      this.isLoading = false;
+    },
+    error: (err) => {
+      this.errorMessage = err.message;
+    },
+    complete: () => {
+      console.log('Done');
+    }
+  }
 
   constructor(private boardService: BoardService, private router: Router) {}
 
   ngOnInit(): void {
-    this.boardService.fetchPosts().subscribe({
-      next: (response) => {
-        this.posts = response;
-        this.isLoading = false;
-      },
-      error: (err) => {
-        this.errorMessage = err.error;
-      },
-    });
+    this.boardSub = this.boardService.fetchPosts().subscribe(this.boardObs);
   }
 
   postDetails(id: String): void {
     this.router.navigate([`post/${id}`]);
+  }
+
+  ngOnDestroy() {
+    this.boardSub?.unsubscribe();
   }
 }
